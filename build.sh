e1073f2d41f5275c8a4d4a327df6654f0b272c8b
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-03-31 23:24:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -v # show current command
 echo pwd:`pwd`
-ls
+ls -l
+cd ..
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=`pwd`/depot_tools:$PATH
 export
