e21e1084959ad1b16c7f0c73ed39f9f9fe591f1e
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-01 00:01:47 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -v # show current command
+echo pwd:`pwd`
+ls -l
+cd ..
+pwd
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+ls -l
+export PATH=`pwd`/depot_tools:$PATH
+export
+gclient
+
+echo "Setup done!"
