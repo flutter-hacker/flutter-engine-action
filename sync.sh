6633d31b085b03c4ebfec03d38ae8b4a23a60168
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-01 00:01:09 +0800

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -v # show current command
+echo pwd:`pwd`
+ls -l
+cd ..
+
+mkdir flutter_engine && cd flutter_engine
+cat > .gclient << "EOF"
+solutions = [
+  {
+    "managed": False,
+    "name": "src/flutter",
+    "url": "https://github.com/flutter/engine.git",
+    "custom_deps": {},
+    "deps_file": "DEPS",
+    "safesync_url": "",
+  },
+]
+EOF
+
+gclient sync -D -f
+echo "gclient sync finished!"
