993a95fb7e097a7ae9283e7949a0cbafd277cd91
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-01 00:13:43 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,28 +5,8 @@ echo pwd:`pwd`
 ls -l
 cd ..
 pwd
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-ls -l
-export PATH=`pwd`/depot_tools:$PATH
-export
-gclient
 
-mkdir flutter_engine && cd flutter_engine
-cat > .gclient << "EOF"
-solutions = [
-  {
-    "managed": False,
-    "name": "src/flutter",
-    "url": "https://github.com/flutter/engine.git",
-    "custom_deps": {},
-    "deps_file": "DEPS",
-    "safesync_url": "",
-  },
-]
-EOF
-
-gclient sync -D -f
-cd src/flutter
+cd flutter_engine/src/flutter
 tools/gn --no-goma --no-prebuilt-dart-sdk --no-lto --unopt
 ninja -C ../out/host_debug_unopt
 
