c793be65aafa0776598442b7935852748adcc26d
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-03-31 23:47:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,24 @@ ls -l
 export PATH=`pwd`/depot_tools:$PATH
 export
 gclient
+
+mkdir flutter_engine && cd flutter_engine
+cat > .gclient << "EOF"
+solutions = [
+  {
+    "managed": False,
+    "name": "src/flutter",
+    "url": "git@github.com:flutter/engine.git",
+    "custom_deps": {},
+    "deps_file": "DEPS",
+    "safesync_url": "",
+  },
+]
+EOF
+
+gclient sync -D -f
+cd src/flutter
+tools/gn --no-goma --no-prebuilt-dart-sdk --no-lto --unopt
+ninja -C ../out/host_debug_unopt
+
+echo "All Done!"
