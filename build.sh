f93aa7f10f6053ac64af54dc75ac8b741e6d3cd3
flutter-hacker <102724685+flutter-hacker@users.noreply.github.com>
2022-04-02 14:18:23 +0800

    Update build_linux.yml

--- build.sh
+++ build.sh
@@ -11,4 +11,12 @@ cd flutter_engine/src/flutter
 tools/gn --no-goma --no-lto --unopt
 ninja -C ../out/host_debug_unopt
 
+df -h
+
+mv ../out $RUNNER_WORKSPACE
+
+rm -r $RUNNER_WORKSPACE/flutter_engine
+
+df -h
+
 echo "Build done!"
